aa77c01781e98122acbff1d4b6fd12cc13cd0e91
shivi168 <77464135+shivi168@users.noreply.github.com>
2023-10-07 16:39:41 -0400

    Update registration_page.dart

--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:justmissed/screens/confirmation_page.dart';
 
-class RegistrationPage extends StatelessWidget {
+class RegistrationPage extends StatefulWidget {
+  @override
+  _RegistrationPageState createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
   // Define variables to store user input
   String email = '';
   String username = '';
   String firstName = '';
   String lastName = '';
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  // Function to validate email format
+  bool isValidEmail(String? email) {
+    if (email == null || email.isEmpty) {
+      return false;
+    }
+    // You can use a regular expression to validate the email format
+    final RegExp emailRegExp = RegExp(r'^[a-zA-Z0-9._-]+@[a-zA-Z]+\.[a-zA-Z]+');
+    return emailRegExp.hasMatch(email);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,72 +33,110 @@ class RegistrationPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Email TextField
-            TextFormField(
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            SizedBox(height: 10),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Email TextField
+              TextFormField(
+                onChanged: (value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  } else if (!isValidEmail(value)) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
 
-            // Username TextField
-            TextFormField(
-              onChanged: (value) {
-                username = value;
-              },
-              decoration: InputDecoration(labelText: 'Username'),
-            ),
-            SizedBox(height: 10),
+              // Username TextField
+              TextFormField(
+                onChanged: (value) {
+                  setState(() {
+                    username = value;
+                  });
+                },
+                decoration: InputDecoration(labelText: 'Username'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your username';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
 
-            // First Name TextField
-            TextFormField(
-              onChanged: (value) {
-                firstName = value;
-              },
-              decoration: InputDecoration(labelText: 'First Name'),
-            ),
-            SizedBox(height: 10),
+              // First Name TextField
+              TextFormField(
+                onChanged: (value) {
+                  setState(() {
+                    firstName = value;
+                  });
+                },
+                decoration: InputDecoration(labelText: 'First Name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your first name';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
 
-            // Last Name TextField
-            TextFormField(
-              onChanged: (value) {
-                lastName = value;
-              },
-              decoration: InputDecoration(labelText: 'Last Name'),
-            ),
-            SizedBox(height: 20),
+              // Last Name TextField
+              TextFormField(
+                onChanged: (value) {
+                  setState(() {
+                    lastName = value;
+                  });
+                },
+                decoration: InputDecoration(labelText: 'Last Name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your last name';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
 
-            // Register Button
+              // Register Button
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Perform registration logic here
+                    print('Email: $email');
+                    print('Username: $username');
+                    print('First Name: $firstName');
+                    print('Last Name: $lastName');
 
-            ElevatedButton(
-              onPressed: () {
-                // Perform registration logic here
-                print('Email: $email');
-                print('Username: $username');
-                print('First Name: $firstName');
-                print('Last Name: $lastName');
-
-                // Navigate to the confirmation page and pass data
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ConfirmationPage(
-                      email: email,
-                      username: username,
-                      firstName: firstName,
-                      lastName: lastName,
-                    ),
-                  ),
-                );
-              },
-              child: Text('Register'),
-            ),
-          ],
+                    // Navigate to the confirmation page and pass data
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ConfirmationPage(
+                          email: email,
+                          username: username,
+                          firstName: firstName,
+                          lastName: lastName,
+                        ),
+                      ),
+                    );
+                  }
+                },
+                child: Text('Register'),
+              ),
+            ],
+          ),
         ),
       ),
     );
