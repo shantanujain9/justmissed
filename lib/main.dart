b1cc2ac582e4be6b4c8f9bed480c406e06caa253
shivi168 <77464135+shivi168@users.noreply.github.com>
2023-10-07 14:05:54 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:justmissed/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,58 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Connect',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // Here, we set the initial route to the AuthenticationScreen.
-      home: const AuthenticationScreen(),
-    );
-  }
-}
-
-// This is a placeholder for your Authentication screen
-class AuthenticationScreen extends StatelessWidget {
-  const AuthenticationScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Authentication')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const HomeScreen()),
-            );
-          },
-          child: const Text('Go to Home'),
-        ),
-      ),
-    );
-  }
-}
-
-// This is a placeholder for your Home screen
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-            Text('Welcome to Home Screen!'),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text("Go back to first page"))
-          ])),
+      home: LoginPage(),
     );
   }
 }
